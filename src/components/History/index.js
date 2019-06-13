e787076edb2b606dba862f95ef33eed57ecd6d09
Kevin Westphal <kevinwestphal@alum.calarts.edu>
2019-06-13 16:39:00 +0200

    Request History working

--- src/components/History/index.js
+++ src/components/History/index.js
@@ -2,22 +2,13 @@ import React, { Component } from 'react';
 import { compose } from 'recompose';
 
 import { withFirebase } from '../Firebase';
-import { withAuthorization, AuthUserContext } from '../Session';
+import { withAuthentication, withAuthorization } from '../Session';
 
-/*const History = () => (
-  <AuthUserContext.Consumer>
-	{authUser => (
-	  <div>
-	    <h1>Request History</h1>
-	  </div>
-	)}
-  </AuthUserContext.Consumer>
-);*/
 
 const INITIAL_STATE = {
   loading: false,      
   requests: [],
-  authUser: {},
+  authUser: JSON.parse(localStorage.getItem('authUser')),
 };
 
 let noRequests = true;
@@ -30,36 +21,31 @@ class History extends Component {
   }
 
   componentDidMount() {
-	  this.listener = this.props.firebase.onAuthUserListener(
-      authUser => {
-        this.setState({ authUser });
-      },
-      () => {
-        this.setState({ authUser: null });
-      },
-    );
-
     this.setState({ loading: true });
 
+    const { authUser } = this.state;
+
     this.props.firebase
-    	.user(this.state.authUser.uid)
+    	.user(authUser.uid)
     	.child('requests')
     	.on('value', snapshot => {
-      		const requestsObject = snapshot.val();
-//TODO
-        //  if (requestsObject != null) {
-        		const requestsList = Object.keys(requestsObject).map(key => ({
-          	...requestsObject[key],
-          	id: key,
-            }));
+  		  const requestsObject = snapshot.val();
 
-            this.setState({ requests: requestsList });
-            noRequests = false;
-          //}
+        //if(requestsObject != null) {
+      		const requestsList = Object.keys(requestsObject).map(key => ({
+        	...requestsObject[key],
+        	uid: key,
+          }));
 
-      this.setState({
-        loading: false,
-      });
+          this.setState({
+            requests: requestsList, 
+            noRequests: false,         
+          });
+       // }
+
+        this.setState({
+          loading: false,
+        });
     });
   }
 
@@ -73,44 +59,51 @@ class History extends Component {
   render() {
     const { requests, loading } = this.state;
 
+    const RequestList = ({ requests }) => (
+        <div>
+          {loading == false &&
+            <ul>
+              {requests.map(request => (
+                <li key={request.id}>
+                  <span>
+                    <strong>ID:</strong> {request.id}
+                  </span>
+                  <span>
+                    <strong>E-Mail:</strong> {request.rcpt_name}
+                  </span>
+                  <span>
+                    <strong>Username:</strong> {request.rcpt_name}
+                  </span>
+                  <span>
+                    <strong>Real Name:</strong> {request.rcpt_name}
+                  </span>
+                  <span>
+                    <strong>Address:</strong> {request.rcpt_name}
+                  </span>
+                </li>
+              ))}
+            </ul>
+          }
+          {loading &&
+            <div>
+              <h1>Loading...</h1>  
+            </div>
+          }
+        </div>
+    );
+
     return (
       <div>
-        {noRequests && !loading &&
-          <p>You have not made any requests, yet.</p>
-        } 
-        {!noRequests &&
-          <RequestList />
-        }    
+        <RequestList requests={requests}/>   
       </div>
     );
   }
 }
 
-const RequestList = ({ requests }) => (
-  <ul>
-    {requests.map(request => (
-      <li key={request.id}>
-        <span>
-          <strong>ID:</strong> {request.id}
-        </span>
-        <span>
-          <strong>E-Mail:</strong> {request.rcpt_name}
-        </span>
-        <span>
-          <strong>Username:</strong> {request.rcpt_name}
-        </span>
-        <span>
-          <strong>Real Name:</strong> {request.rcpt_name}
-        </span>
-        <span>
-          <strong>Address:</strong> {request.rcpt_name}
-        </span>
-      </li>
-    ))}
-  </ul>
-);
+const condition = authUser => !!authUser;
 
-//const condition = authUser => !!authUser;
-
-//withAuthorization(condition)(History);
-export default withFirebase(History);
+export default compose(
+  withAuthorization(condition),
+  withAuthentication,
+  withFirebase,
+)(History);
