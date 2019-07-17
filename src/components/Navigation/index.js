225ee7e4538d358035ba9edb4a6ff70561545574
Kevin Westphal <kevinwestphal@alum.calarts.edu>
2019-07-17 17:19:43 +0200

    Still styling

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -3,38 +3,40 @@ import { Link } from 'react-router-dom';
 
 import SignOutButton from '../SignOut';
 import * as ROUTES from '../../constants/routes';
-import { AuthUserContext } from '../Session';
+import { AuthUserContext, withAuthorization } from '../Session';
 import * as ROLES from '../../constants/roles';
 
 
 const Navigation = ({ authUser }) => (
   <div>
-    <ul>
-      <li>
-        <Link to={ROUTES.LANDING}>Landing</Link>
-      </li>
-      <li>
-        <Link to={ROUTES.HOME}>Home</Link>
-      </li>
-      <li>
-        <Link to={ROUTES.START_REQUEST}>Start New Request</Link>
-      </li>
-      <li>
-        <Link to={ROUTES.HISTORY}>Request History</Link>
-      </li>
-      <li>
-        <Link to={ROUTES.ACCOUNT}>Account</Link>
-      </li>
-      {!!authUser.roles[ROLES.ADMIN] && (
+    <AuthUserContext.Consumer>
+      <ul>
         <li>
-          <Link to={ROUTES.ADMIN}>Admin</Link>
+          <Link to={ROUTES.LANDING}>Landing</Link>
         </li>
-      )}
-      <li>
-        <SignOutButton />
-      </li>
-    </ul>
+        <li>
+          <Link to={ROUTES.HOME}>Home</Link>
+        </li>
+        <li>
+          <Link to={ROUTES.START_REQUEST}>Start New Request</Link>
+        </li>
+        <li>
+          <Link to={ROUTES.HISTORY}>Request History</Link>
+        </li>
+        <li>
+          <Link to={ROUTES.ACCOUNT}>Account</Link>
+        </li>
+        {!!authUser.roles[ROLES.ADMIN] && (
+          <li>
+            <Link to={ROUTES.ADMIN}>Admin</Link>
+          </li>
+        )}
+        <li>
+          <SignOutButton />
+        </li>
+      </ul>
+    </AuthUserContext.Consumer>
   </div>
 );
 
-export default Navigation;
+export default withAuthorization(Navigation);
